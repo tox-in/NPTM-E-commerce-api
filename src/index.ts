f0f89e683e309799e2719b37660a8231cd718883
tox-in <habimanat78@gmail.com>
2024-09-29 10:12:04 +0200

    directory structure

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
-import express from 'express';
+import express, {Express, Request, Response} from 'express';
 
 const app = express();
+
+app.get('/', (req:Request, res: Response) => {
+    res.send('Working')
+})
 
 app.listen(3000, () => {console.log('App running on http://localhost:3000');
 })
