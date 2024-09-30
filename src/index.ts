dd0d9a483ca0254e39f48631d2e6897335b01e60
tox-in <habimanat78@gmail.com>
2024-09-30 09:19:05 +0200

    changes added

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express, {Express, Request, Response} from 'express';
 import { PORT } from './secrets';
 import rootRouter from './routes';
+import { PrismaClient } from '@prisma/client';
 
 const app = express();
 
 app.use('/api', rootRouter);
+
+export const prismaClient = new PrismaClient({
+    log:['query']
+})
 
 app.listen(PORT, () => {console.log(`App running on http://localhost:${PORT}`);
 })
